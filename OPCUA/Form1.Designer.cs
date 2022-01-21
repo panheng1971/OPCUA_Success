df554373aaf67cac69241032d0480e7eec9cdab9
panheng1971 <pan.heng@hotmail.com>
2022-01-21 11:49:38 +0800

    Add project files.

---
+++ OPCUA/Form1.Designer.cs
@@ -0,0 +1,199 @@
+﻿namespace OPCUA
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.btBrowseSever = new System.Windows.Forms.Button();
+            this.btReadNest = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(93, 32);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "连接";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(224, 32);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "断开";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(93, 92);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 27);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "read";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(93, 156);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 27);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "write";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("SimSun", 12F);
+            this.textBox1.Location = new System.Drawing.Point(224, 92);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(167, 26);
+            this.textBox1.TabIndex = 4;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("SimSun", 12F);
+            this.textBox2.Location = new System.Drawing.Point(224, 156);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(167, 26);
+            this.textBox2.TabIndex = 5;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // btBrowseSever
+            // 
+            this.btBrowseSever.Location = new System.Drawing.Point(93, 222);
+            this.btBrowseSever.Name = "btBrowseSever";
+            this.btBrowseSever.Size = new System.Drawing.Size(116, 23);
+            this.btBrowseSever.TabIndex = 6;
+            this.btBrowseSever.Text = "BrowseSever";
+            this.btBrowseSever.UseVisualStyleBackColor = true;
+            this.btBrowseSever.Click += new System.EventHandler(this.btBrowseSever_Click);
+            // 
+            // btReadNest
+            // 
+            this.btReadNest.Location = new System.Drawing.Point(93, 324);
+            this.btReadNest.Name = "btReadNest";
+            this.btReadNest.Size = new System.Drawing.Size(75, 23);
+            this.btReadNest.TabIndex = 7;
+            this.btReadNest.Text = "ReadRest";
+            this.btReadNest.UseVisualStyleBackColor = true;
+            this.btReadNest.Click += new System.EventHandler(this.btReadNest_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(197, 324);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(102, 21);
+            this.textBox3.TabIndex = 8;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(110, 391);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 9;
+            this.button5.Text = "button5";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(110, 449);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 23);
+            this.button6.TabIndex = 10;
+            this.button6.Text = "button6";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // timer2
+            // 
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1110, 543);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.btReadNest);
+            this.Controls.Add(this.btBrowseSever);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button btBrowseSever;
+        private System.Windows.Forms.Button btReadNest;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Timer timer2;
+    }
+}
+
---
+++ OPCUA/Form1.cs
@@ -0,0 +1,202 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using OpcUaHelper;
+using OpcUaHelper.Forms;
+using RestSharp;
+using RestSharp.Authenticators;
+using System.Net;
+using System.Net.Security;
+using System.Security.Cryptography.X509Certificates;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace OPCUA
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private OpcUaClient opcUaClient = new OpcUaClient();
+        private async void button1_Click(object sender, EventArgs e)
+        {
+           
+
+            try
+            {
+
+                //opcUaClient.UserIdentity = new Opc.Ua.UserIdentity("administrator", "123456");//
+                //await opcUaClient.ConnectServer("opc.tcp://192.168.1.2:49320");
+                await opcUaClient.ConnectServer("opc.tcp://office:4862");
+                //MessageBox.Show("连接成功！");
+                button1.BackColor = Color.Green;
+                button2.BackColor = Color.LightGray;
+        
+            }
+            catch(Exception ex)
+            {
+                ClientUtils.HandleException(Text, ex);
+            }
+           
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            opcUaClient.Disconnect();
+            button1.BackColor = Color.LightGray;
+            button2.BackColor = Color.Red;
+        }
+        
+
+    private void button3_Click(object sender, EventArgs e)
+        {
+
+            if (!opcUaClient.Connected)
+            {
+                MessageBox.Show("请先连接OPC服务器！");
+                return;
+            }
+            this.timer1.Enabled = true;
+            //try
+            //{
+            //    Opc.Ua.DataValue value = opcUaClient.ReadNode("ns=2;s=模拟器示例.函数.Ramp1");
+            //    textBox1.Text = value.ToString(); // 显示测试数据
+            //}
+            //catch (Exception ex)
+            //{
+            //    // 使用了opc ua的错误处理机制来处理错误，网络不通或是读取拒绝
+            //    ClientUtils.HandleException(Text, ex);
+            //}
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                bool IsSuccess = opcUaClient.WriteNode("ns=2;s=模拟器示例.函数.Ramp1", 123);
+                textBox2.Text = IsSuccess.ToString(); ; // 显示True，如果成功的话
+            }
+            catch (Exception ex)
+            {
+                // 使用了opc ua的错误处理机制来处理错误，网络不通或是读取拒绝
+                ClientUtils.HandleException(Text, ex);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (!opcUaClient.Connected) { textBox1.Text = "连接已经断开！"; 
+                return; }
+            try
+            {
+                Opc.Ua.DataValue value = opcUaClient.ReadNode("ns=1;s=t|MyTagFloat");
+                textBox1.Text = value.ToString(); // 显示测试数据
+            }
+            catch (Exception ex)
+            {
+                // 使用了opc ua的错误处理机制来处理错误，网络不通或是读取拒绝
+                ClientUtils.HandleException(Text, ex);
+            }
+        }
+
+        private void btBrowseSever_Click(object sender, EventArgs e)
+        {
+            using (FormBrowseServer form = new FormBrowseServer())
+            {
+                form.ShowDialog();
+            }
+        }
+
+        private void btReadNest_Click(object sender, EventArgs e)
+        {
+
+            this.timer2.Enabled = true;
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            HttpWebRequest req = WebRequest.Create("https://office:34568/WinCCRestService/tagManagement/Value/MyTagFloat") as HttpWebRequest;
+
+            //HttpWebRequest req = WebRequest.Create("https://office:34568/WinCCRestService") as HttpWebRequest;
+
+            ServicePointManager.ServerCertificateValidationCallback = new RemoteCertificateValidationCallback(CheckValidationResult);
+            req.Method = "GET";
+            //req.Headers.Add("Content-Type", "application/json");
+
+            HttpWebResponse res = (HttpWebResponse)req.GetResponse();
+            Stream str = res.GetResponseStream();
+            StreamReader myStreamReader = new StreamReader(str, Encoding.UTF8);
+            string retString = myStreamReader.ReadToEnd();
+
+            MessageBox.Show(str.ToString());
+
+        }
+        private static bool CheckValidationResult(object sender, X509Certificate certificate, X509Chain chain, SslPolicyErrors errors)
+        {
+            if (errors == SslPolicyErrors.None)
+                return true;
+            return true;
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            //HttpWebRequest req = WebRequest.Create("https://office:34568/WinCCRestService/tagManagement/Value/MyTagFloat") as HttpWebRequest;
+
+            HttpWebRequest req = WebRequest.Create("https://office:34568/WinCCRestService") as HttpWebRequest;
+
+            ServicePointManager.ServerCertificateValidationCallback = new RemoteCertificateValidationCallback(CheckValidationResult);
+            req.Method = "POST";
+            req.ContentLength =0;
+            //req.Headers.Add("Content-Type", "application/json");
+
+
+            HttpWebResponse res = (HttpWebResponse)req.GetResponse();
+            Stream str = res.GetResponseStream();
+            StreamReader myStreamReader = new StreamReader(str, Encoding.UTF8);
+            string retString = myStreamReader.ReadToEnd();
+
+            MessageBox.Show(retString.ToString());
+
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            RestClient client = new RestClient("https://office:34568/WinCCRestService/tagManagement/Value/MyTagFloat");
+            client.Authenticator = new HttpBasicAuthenticator("admin", "123456");
+            var request = new RestRequest("statuses/home_timeline.json", DataFormat.Json);
+            ServicePointManager.ServerCertificateValidationCallback = new RemoteCertificateValidationCallback(CheckValidationResult);
+            request.AddHeader("Content-Type", "application/json");
+            request.Timeout = 10000;
+            request.AddHeader("Cache-Control", "no-cache");
+            
+            IRestResponse response = client.Execute(request);
+
+            var content = response.Content;
+            MyValue value = JsonConvert.DeserializeObject<MyValue>(content);
+            
+            textBox3.Text =String.Format("{0:0.00}",value.value);
+        }
+    }
+    public class MyValue
+    {
+        public string Tagname { get; set; }
+        public string datatype { get; set; }
+        public float value { get; set; }
+        public string timestamp { get; set; }
+        public string qualitycode { get; set; }
+    }
+}
